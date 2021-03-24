480f14bc76f189c8181033baf7e48df501e2466c
febasusansunny <febasusansunny@users.noreply.github.com>
2021-03-24 13:59:02 +0530

    Add files via upload

---
+++ src/pages/usersprofile/usersProfile.Data.js
@@ -0,0 +1,459 @@
+const USERS_DATA = [
+    {
+      
+      id: 0,
+       
+        details_section: [
+          {
+              name: 'Mr Janique Costa',
+              gender: "Male",
+              city: "Araraquara",
+              state: "Rondônia",
+              country: "Brazil",
+              age:"30",
+              email: "janique.costa@example.com",
+              phone: "(84) 5181-4592",
+              father:"Lison",
+              mother:"Linu",
+              occupation:"Doctor",
+              imageUrl: 'https://st4.depositphotos.com/12982378/22072/i/600/depositphotos_220729084-stock-photo-smiling-adult-man-crossed-arms.jpg',
+              },
+              
+        ]
+      },
+      {
+        id: 1,
+       
+        details_section: [
+          {
+              name: 'Mr Janique Costa',
+              gender: "Male",
+              city: "Araraquara",
+              state: "Rondônia",
+              country: "Brazil",
+              age:"30",
+              email: "janique.costa@example.com",
+              phone: "(84) 5181-4592",
+              father:"Lison",
+              mother:"Linu",
+              occupation:"Doctor",
+              imageUrl: 'https://st4.depositphotos.com/12982378/22072/i/600/depositphotos_220729084-stock-photo-smiling-adult-man-crossed-arms.jpg',
+              },
+              
+        ]
+      },
+      {
+        id: 2,
+        
+        details_section: [
+          {
+              name: 'Mr Khaled Børresen',
+              gender: "Male",
+              city: "Norheimsund",
+              country: "Denmark",
+              state: "Akershus",
+              age:"30",
+              email: "khaled.borresen@example.com",
+              phone: "78129723",
+              father:"John",
+              mother:"Karly",
+              occupation:"Software Engineer",
+              imageUrl:'https://www.mobolize.com/wp/wp-content/uploads/2018/05/man-tablet-smiling-600x400.jpg',
+          },
+              
+        ]
+      },
+      {
+        id:3 ,
+        details_section: [
+          {
+            name: 'Miss Maja Kristensen',
+              gender: "Female",
+             city: "Haslev",
+              state: "Sjælland",
+              country: "Denmark",
+              email: "maja.kristensen@example.com",
+              age: "33",
+              phone: "14295904",
+              father:"John",
+              mother:"Karly",
+              occupation:"Mechanical Engineer",
+              imageUrl:'https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half-caption/public/field_blog_entry_images/2018-09/shutterstock_648907024.jpg?itok=0hb44OrI',
+              },
+              
+        ]
+      },
+      {
+        id: 4,
+      
+       
+        details_section: [
+          {
+            name: 'Mrs Latife Cevik',
+              gender: "Female",
+             city: "Iğdır",
+              state: "Isparta",
+              country: "Turkey",
+              Email: "latife.cevik@example.com",
+              age: "35",
+             phone: "(385)-741-1739",
+             father:"Jian",
+             mother:"Merrly",
+             occupation:"Mechanical Engineer",
+             imageUrl:'https://cdn.pixabay.com/photo/2015/12/19/21/03/person-1100286_960_720.jpg'
+              },
+              
+        ]
+      },
+      {
+        id: 5,
+        
+        routeName: 'name5',
+        details_section: [
+          {
+            name: 'Mrs Sayenne Leendertse',
+            gender: "Female",  
+             city: "Zieuwent",
+              state: "Noord-Brabant",
+              country: "Netherlands",
+              email: "sayenne.leendertse@example.com",
+              age: "54",
+             phone: "(056)-707-5483",
+             father:"Deyam",
+             mother:"Merry",
+             occupation:"Business Analyst",
+             imageUrl: 'https://img.freepik.com/free-photo/cheerful-curly-business-girl-wearing-glasses_176420-206.jpg?size=626&ext=jpg'
+              },
+              
+        ]
+      },
+      {
+          id: 6,
+        
+          details_section: [
+              {
+                name: 'Mr Niklas Hamalainen',
+                gender: "Male",
+               
+               city: "Pieksämäki",
+                state: "Pirkanmaa",
+                country: "Finland",
+                email: "niklas.hamalainen@example.com",
+                age: 35,
+               phone: "02-467-751",
+               father:"Deyam",
+             mother:"Merry",
+             occupation:"Business Analyst",
+               imageUrl: 'https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg'
+                  },
+                  
+          ]
+        },
+        {
+          id: 7,
+         
+          details_section: [
+              { 
+                name: 'Mr Friedlinde wurfel',
+                gender: "Male",
+               city: "Lennestadt",
+                state: "Sachsen-Anhalt",
+                Country: "Germany",
+                email: "friedlinde.wurfel@example.com",
+                age: 63,
+                father:"Jian",
+                mother:"Merrly",
+                occupation:"Mechanical Engineer",
+               phone: "0366-3579098",
+               imageUrl:'https://img.freepik.com/free-photo/cheerful-businessman-enjoying-success_1262-16166.jpg?size=626&ext=jpg'
+              }
+                  
+          ]
+        },
+        {
+        id: 8,
+          
+         
+          details_section: [
+              {
+                name: 'Mr Freja Madsen',
+                  gender: "Male",
+                  
+                 city: "Øster Assels",
+                  state: "Syddanmark",
+                  country: "Denmark",
+                  email: "freja.madsen@example.com",
+                  age: 36,
+                 phone: "29069464",
+                 father:"Jian",
+                 mother:"Merrly",
+                 occupation:"Engineer",
+                 imageUrl:'http://st.depositphotos.com/2931363/4881/i/450/depositphotos_48813801-Businessman-pointing-copy-space..jpg'
+                  }
+                  
+                  
+          ]
+        },
+        {
+          id:9 ,
+          details_section: [
+              {
+                name: 'Miss Florence harris',
+                  gender: "Female",
+                 city: "Kingston",
+                  state: "Ontario",
+                  country: "Canada",
+                  email: "florence.harris@example.com",
+                  age: 31,
+                 phone: "879-267-3395",
+                 father:"Obama",
+                 mother:"Michelle",
+                 occupation:"System Analyst",
+                 father:"Jian",
+                 mother:"Merrly",
+                 occupation:"Engineer",
+                 imageUrl:'https://img.freepik.com/free-photo/indoor-shot-beautiful-happy-african-american-woman-smiling-cheerfully-keeping-her-arms-folded-relaxing-indoors-after-morning-lectures-university_273609-1270.jpg?size=626&ext=jpg&ga=GA1.2.1660430068.1601510400'
+                  },
+                  
+          ]
+        },
+        {
+          id: 10,
+          details_section: [
+              {
+                name: 'Mrs Monsieur Reto Chevalier',
+                  gender: "Female", 
+                 city: "Épendes (Vd)",
+                  state: "Bern",
+                  country: "Switzerland",
+                  
+                  age:34,
+                   email: "Monsieur.Reto@example.com",
+                  father:"Deyam",
+                  mother:"Merry",
+                  occupation:"Business Analyst",
+                  
+                 phone: "076 561 25 98",
+                 imageUrl:'https://image.freepik.com/free-photo/surprised-playful-touched-good-looking-african-american-woman-glasses-stylish-brown-t-shirt-clasping-hands-smiling-from-joy-excitement-liking-great-show_176420-23322.jpg'
+                  },
+          ]
+        },
+        { id: 11,
+          
+          
+          details_section: [
+              {
+                 name: 'Mr Stenan Dufour',
+                  gender: "Male",
+                  
+                 city: "Nîmes",
+                  state: "Aveyron",
+                  country: "France",
+                
+                  email: "stella.dufour@example.com",
+                  father:"Denin",
+                  mother:"Maxena",
+                  occupation:"Business Analyst",
+                 
+                age: 34,
+                
+                 phone: "0245755920",
+                 imageUrl: 'https://st3.depositphotos.com/12985790/16875/i/600/depositphotos_168751754-stock-photo-man-buttoning-up-shirt.jpg',
+                  },
+                ]
+          
+        },
+        {
+          id: 12,
+          details_section: [
+              {
+                name: 'Mr mohamed santos',
+                  gender: "Male",
+                 city: "Santa Cruz de Tenerife",
+                  state: "Aragón",
+                  country: "Spain",
+                  email: "mohamed.santos@example.com",
+                  father:"Jenin",
+                  mother:"Maxena",
+                  occupation:"Account Analyst",
+                  age: 38,
+                 phone: "934-797-922",
+                 imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlCogB5Hd9JblmBc1a8vbNgkzdW5l6dvzV3Q&usqp=CAU',
+                  },
+                  
+          ]
+        },
+        {
+          id: 13,
+        
+          details_section: [
+              {
+                name: 'Mr Jonathan Hansen',
+                  gender: "Male",
+                   city: "Pandrup",
+                  state: "Midtjylland",
+                  country: "Denmark",
+                  email: "jonathan.hansen@example.com",
+                 phone: "20837803",
+                 father:"Dion Dusk",
+                 mother:"Harris",
+                 occupation:"Nurse",
+                 imageUrl: 'https://st3.depositphotos.com/10654668/14328/i/600/depositphotos_143284773-stock-photo-businessman-with-cup-of-coffee.jpg',
+              }  
+                 
+          ]
+        },
+        {
+          id: 14,
+          details_section: [
+              {
+                name: 'Mr Henri Penner',
+                  gender: "Male",
+                 city: "Ruhland",
+                  state: "Baden-Württemberg",
+                  country: "Germany",
+                
+                  email: "henri.penner@example.com",
+                  age: 71,
+                  father:"Dion Dusk",
+                 mother:"Harris",
+                 occupation:"Medical Representative",
+                 phone: "0366-6429968",
+                 imageUrl: 'https://us.123rf.com/450wm/vadymvdrobot/vadymvdrobot1903/vadymvdrobot190306154/119989030-portrait-of-a-beautiful-pensive-young-woman-casualy-dressed-standing-isolated-over-white-background.jpg?ver=6',
+                  }
+          ]
+        },
+        {
+          id: 15,
+         
+          details_section: [
+              {
+                name: 'Ms Linda Rogers',
+                gender: "Female",
+               city: "Ballybofey-Stranorlar",
+                state: "Kilkenny",
+                country: "Ireland",
+                email: "linda.rogers@example.com",
+                age: 31,
+               Phone: "051-144-6220",
+               father:"Deyam",
+                mother:"Merry",
+               occupation:"Assistant Professor",
+               imageUrl: 'https://st3.depositphotos.com/1037987/15097/i/600/depositphotos_150975580-stock-photo-portrait-of-businesswoman-in-office.jpg',
+                  },
+                 
+          ]
+        },
+        {
+          id: 16,
+         
+         
+          details_section: [
+              {
+                name: 'Miss Doreen Krull',
+                  gender: "Female",
+                 city: "Rüthen",
+                  state: "Brandenburg",
+              
+                  email: "doreen.krull@example.com",
+                
+                  age: 66,
+                
+                 phone: "0760-2230454",
+                 father:"Deyam",
+                 mother:"Merry",
+                occupation:"Assistant Professor",
+                imageUrl: 'https://assets.entrepreneur.com/content/3x2/2000/20161214164224-GettyImages-533978993.jpeg'
+              }
+                 
+          ]
+        },
+        {
+          id: 17,
+          details_section: [
+              {
+                name: 'Miss Elke Wolk',
+                  gender: "Female",
+                 city: "Zwickauer Land",
+                  state: "Baden-Württemberg",
+                  country: "Germany",
+                  age: 27,
+                 phone: "0972-6918445",
+                 father:"Michael Thomas",
+                 mother:"Merry",
+                occupation:"Assistant Professor",
+              imageUrl:  'https://thumbs.dreamstime.com/b/close-up-portrait-rejoicing-surprised-asian-woman-glasses-happy-person-hear-great-awesome-news-realise-something-close-178164807.jpg'
+                  },
+                  
+          ]
+        },
+        {
+          id: 18,
+          details_section: [
+              {
+                name: 'Mr Logan Hamilton',
+                  gender: "Male",
+                 city: "Balbriggan",
+                  state: "Cork",
+                  country: "Ireland",
+                  email: "logan.hamilton@example.com",
+                  age: 55,
+                 phone: "021-056-6600",
+                 father:"Thomas Kennedy",
+                 mother:"Jain",
+                occupation:"Software Developer",
+                imageUrl: 'https://content.api.news/v3/images/bin/a6923adbc7bece73803221613f410782',
+                  },
+                  
+          ]
+        },
+        {
+          id: 19,
+          name: 'Ms Latif Naha',
+          details_section: [
+              {
+                name: 'Ms Latif Naha',
+                  gender: "Female",
+                 city: "رشت",
+                  state: "کردستان",
+                  country: "Iran",
+                  email: "ysmn.khmrw@example.com",
+                  age: 62,
+                 phone: "019-36742265",
+                 father:"Varghese Kennedy",
+                 mother:"Jain",
+                occupation:"Software Developer",
+                imageUrl: 'https://www.incimages.com/uploaded_files/image/1920x1080/getty_143919450_970647970450097_58844.jpg',
+                  },
+     
+          ]
+        },
+        
+    
+        {
+          id: 20,
+        
+         
+          details_section: [
+              {
+                name: 'Mrs Anna jorgensen',
+                  gender: "Female",
+                 city: "Nykøbing F",
+                  state: "Syddanmark",
+                  country: "Denmark",
+                  email: "anna.jorgensen@example.com",
+                  age: 70,
+                 phone: "08879551",
+                 father:"Varghese Kennedy",
+                 mother:"Mili",
+                occupation:"Actress",
+                imageUrl: ' https://ak.picdn.net/shutterstock/videos/4634066/thumb/6.jpg',
+                  },
+          ]
+        },
+       
+      
+    ];
+  
+  export default USERS_DATA;
+  
